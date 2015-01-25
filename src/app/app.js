13142b2dc8d641086f786fceb200210f4f2f1401
harjup <pikmin@gmail.com>
2015-01-25 18:42:00 -0500

    Fix broken path in gameDetail.tpl.html, surround app.js with iife

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,22 @@
-'use strict';
+((function(){
+    'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('phSite', [
-  'ngRoute',
-  'phSite.game'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-      .when("/game/:name",
-      {
-          templateUrl: 'app/game/detail/gameDetail.tpl.html',
-          controller: 'GameDetailCtrl'
-      })
-      .when('/gameList', {
-            templateUrl: 'app/game/list/gameList.tpl.html',
-            controller: 'GameListCtrl'
-       })
-      .otherwise({redirectTo: '/gameList'});
-}]);
+    // Declare app level module which depends on views, and components
+    angular.module('phSite', [
+        'ngRoute',
+        'phSite.game'
+    ]).
+        config(['$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when("/game/:name",
+                {
+                    templateUrl: 'app/game/detail/gameDetail.tpl.html',
+                    controller: 'GameDetailCtrl'
+                })
+                .when('/gameList', {
+                    templateUrl: 'app/game/list/gameList.tpl.html',
+                    controller: 'GameListCtrl'
+                })
+                .otherwise({redirectTo: '/gameList'});
+        }]);
+})());
